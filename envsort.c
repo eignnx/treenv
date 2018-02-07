d7994e18f9c5f8699b0d8738bca6eed2dbc8fd66
eignnx <eignnx@gmail.com>
2018-02-07 16:41:23 -0500

    Add fn to count env vars

--- envsort.c
+++ envsort.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
+#include <stdbool.h>     // bool
 
 extern char **environ;
+static bool _env_count_called = false;
+static size_t _env_count = 0;
+
+size_t env_count();
 
 int main()
 {
-    while (*environ != NULL)
-        printf("%s\n", *environ++);
+    
+    printf("There are %zu environment variables defined.", env_count());
+}
+
+size_t env_count()
+{
+    if (!_env_count_called) {
+        _env_count_called = true;
+        for (char **envp = environ; *envp; envp++, _env_count++);
+    }
+    return _env_count;
 }
