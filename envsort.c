93fe404973654614d5a4acaea08697df8c1c9f23
eignnx <eignnx@gmail.com>
2018-02-09 16:18:43 -0500

    Test out tree printing

--- envsort.c
+++ envsort.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>     // malloc
 #include <string.h>     // strdup
 
+#include "simpletreeprint.h"
+
 extern const char **environ;
 static bool _env_count_called = false;
 static size_t _env_count = 0;
@@ -10,22 +12,44 @@ static size_t _env_count = 0;
 size_t env_count();
 void strarrcpy(char **dest, const char **src);
 
+#define Space "    "
+
 int main()
 {
-    printf("There are %zu environment variables defined.", env_count());
+    printf("There are %zu environment variables defined.\n", env_count());
 
     char **envcpy = malloc(env_count() * sizeof(*envcpy));
     strarrcpy(envcpy, environ);
 
-    for (int i = 0; envcpy[i] != NULL; i++)
-        printf("%s\n", envcpy[i]);
+    //for (int i = 0; envcpy[i] != NULL; i++)
+    //    printf("%s\n", envcpy[i]);
+
+    int names = 3;
+    int subvalues = 5;
+
+    for (size_t i = 1; i <= names; i++) {
+        printf(
+            "%sVAR%zu\n",
+            (i != names) ? Tee : Ell,
+            i
+        );
+        for (size_t j = 1; j <= subvalues; j++) {
+            printf(
+                "%s%ssubvalue %zu\n",
+                (i != names) ? Pipe : Space,
+                (j != subvalues) ? Tee : Ell,
+                j
+            );
+        }
+    }
+
 
     // Free each string in `envcpy`.
-    for (char *p = envcpy; p != NULL; p++)
-        free(p);
+    //for (char **p = envcpy; p != NULL; p++)
+    //    free(*p);
 
     // Free array itself.
-    free(envcpy);
+    //free(envcpy);
 }
 
 size_t env_count()
@@ -47,3 +71,4 @@ void strarrcpy(char **dest, const char **src)
         src++, dest++;
     }
 }
+
